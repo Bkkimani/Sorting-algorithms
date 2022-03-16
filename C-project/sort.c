abd5b4b6bb7455690774f7233e6d87a4e6d25f82
Brian Kimani <kimani@yahoo.com>
2022-03-16 16:06:46 +0300

    function added for small data sorting

--- C-project/sort.c
+++ C-project/sort.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-
+void bubble_sort (int a[], int length);
 int main (void)
 {
 int a []= {8, 6,5, 0, 7, 1, 4, 3, 2};
 int length= 10;
+bubble_sort(a, length);
+
+for (int i= 0; i< length; i++)
+printf("a[%d] = %d\n", i, a[i]);
+    return 0;
+}
+void bubble_sort (int a[], int length)
+{
 for (int i= 0; i< length; i++)
 {
     for (int j= 0; j< length- 1; j++)
@@ -16,7 +24,4 @@ if (a [j]> a [j + 1])
 }
     }
 }
-for (int i= 0; i< length; i++)
-printf("a[%d] = %d\n", i, a[i]);
-    return 0;
 }
